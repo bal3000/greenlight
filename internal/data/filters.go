6a5f36819585f87d7cb082e830e89aab3e536b38
bal3000 <bal3000286@googlemail.com>
2021-12-31 16:14:48 +0000

    sorting

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,12 +1,40 @@
 package data
 
-import "github.com/bal3000/greenlight/internal/validator"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/bal3000/greenlight/internal/validator"
+)
 
 type Filters struct {
 	Page         int
 	PageSize     int
 	Sort         string
 	SortSafelist []string
+}
+
+// Check that the client-provided Sort field matches one of the entries in our safelist
+// and if it does, extract the column name from the Sort field by stripping the leading
+// hyphen character (if one exists).
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic(fmt.Sprintf("unsafe sort parameter: %s", f.Sort))
+}
+
+// Return the sort direction ("ASC" or "DESC") depending on the prefix character of the
+// Sort field.
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
