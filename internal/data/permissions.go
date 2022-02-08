113cf379738fa55a838750939b25a2aac4b452e2
bal3000 <bal3000286@googlemail.com>
2022-02-08 16:33:53 +0000

    add permissions

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"time"
+
+	"github.com/lib/pq"
 )
 
 // Hold the permission codes (like "movies:read" and "movies:write") for a single user.
@@ -21,6 +23,7 @@ func (p Permissions) Include(code string) bool {
 
 type PermissionModeler interface {
 	GetAllForUser(userID int64) (Permissions, error)
+	AddForUser(userID int64, codes ...string) error
 }
 
 type PermissionModel struct {
@@ -64,4 +67,17 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 	}
 
 	return permissions, nil
+}
+
+// Add the provided permission codes for a specific user
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `
+		INSERT INTO users_permissions
+		SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, userID, pq.Array(codes))
+	return err
 }
